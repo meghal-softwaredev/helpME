7e57061162705a70e7d337e27f8acbb44bcd7833
Umang Patel <umangpatel1991@gmail.com>
2021-11-14 09:51:23 -0500

    add list items in left sidebar

--- client/src/components/LeftSideBar.js
+++ client/src/components/LeftSideBar.js
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+import "./LeftSideBar.scss";
+
 function LeftSideBar() {
   return (
-    <div>
-      Left sidebar
+    <div className="left-sidebar-container">
+      <ul>
+        <li>
+          <Link to="/feeds">Feeds</Link>
+        </li>
+        <li>
+          <Link to="/sessions">Sessions</Link>
+        </li>
+        <li>
+          <Link to="/post_question">Post Question</Link>
+        </li>
+      </ul>
     </div>
   )
 }
