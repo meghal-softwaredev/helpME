e9f029aad41fffd9b5dc0959e00eb63c40bcb1e4
Umang Patel <umangpatel1991@gmail.com>
2021-11-14 08:30:55 -0500

    re-structure components layout

--- client/src/App.js
+++ client/src/App.js
@@ -2,51 +2,14 @@ import './App.css';
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
 import Register from './components/Register';
 import Signin from './components/Signin';
+import Header from './components/Header';
 
 function App() {
   
   return (
     <BrowserRouter>
       <div className="grid-container">
-        <header className="row">
-          <div>
-            <Link className="brand" to="/">
-              helpME
-            </Link>
-          </div>
-          
-          <div>
-            <Link to="/getHelp">
-              Get Help
-            </Link>
-            {/* {userInfo ? ( */}
-              <div className="dropdown">
-                <Link to="#"> Name
-                  {/* {userInfo.name} <i className="fa fa-caret-down"></i>{' '} */}
-                </Link>
-                <ul className="dropdown-content">
-                  <li>
-                    <Link to="/profile">User Profile</Link>
-                  </li>
-                  <li>
-                    <Link to="#signout">
-                      Sign Out
-                    </Link>
-                  </li>
-                </ul>
-              </div>
-             {/* ) : (  */}
-              <ul>
-                <li>
-                  <Link to="/register">Register</Link>
-                </li>
-                <li>
-                  <Link to="/signin">Sign In</Link>
-                </li>
-              </ul>
-            {/* )}  */}
-          </div>
-        </header>
+        <Header />
         <main>
           <Routes>
             <Route path="/register" element={<Register/>}></Route>
