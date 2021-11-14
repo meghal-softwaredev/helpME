e3ca93d828d71315e092b4ade99b871c699b222c
Meghal <meghal.softwaredev@gmail.com>
2021-11-14 14:54:49 -0500

    Add route to find particular user by id

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -51,4 +51,17 @@ userRouter.post(
     res.status(401).send({ message: 'Invalid email or password' });
   })
 );
+
+userRouter.get(
+  '/:id',
+  expressAsyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id);
+    if (user) {
+      res.send(user);
+    } else {
+      res.status(404).send({ message: 'User Not Found' });
+    }
+  })
+);
+
 export default userRouter;
