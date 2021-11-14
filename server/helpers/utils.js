c8044580c025cbee77fa5005e03bb912f7e87435
Meghal <meghal.softwaredev@gmail.com>
2021-11-14 15:40:33 -0500

    Add sign() and verify() methods for json web token

--- server/helpers/utils.js
+++ server/helpers/utils.js
@@ -25,7 +25,6 @@ export const isAuth = (req, res, next) => {
           res.status(401).send({ message: 'Invalid Token' });
         } else {
           req.user = decode;
-          console.log("decode",decode);
           next();
         }
       }
