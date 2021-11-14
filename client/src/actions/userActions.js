2f1587a4cc13cd4b3f7d6f4e4aedaa29677a08a0
Meghal <meghal.softwaredev@gmail.com>
2021-11-14 13:59:06 -0500

    Add signin constants

--- client/src/actions/userActions.js
+++ client/src/actions/userActions.js
@@ -2,7 +2,10 @@ import Axios from 'axios';
 import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCESS,
-  USER_REGISTER_FAIL
+  USER_REGISTER_FAIL,
+  USER_SIGNIN_REQUEST,
+  USER_SIGNIN_SUCCESS,
+  USER_SIGNIN_FAIL
 } from '../constants/userConstants';
 
 export const register = (name, email, password) => async (dispatch) => {
