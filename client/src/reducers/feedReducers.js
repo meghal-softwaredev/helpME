63a15035a36cf64484e31283b0172e24316f855f
Umang Patel <umangpatel1991@gmail.com>
2021-11-15 00:53:02 -0500

    add reducer for new feed

--- client/src/reducers/feedReducers.js
+++ client/src/reducers/feedReducers.js
@@ -2,6 +2,10 @@ const {
   FEED_LIST_REQUEST,
   FEED_LIST_SUCCESS,
   FEED_LIST_FAIL,
+  FEED_CREATE_REQUEST,
+  FEED_CREATE_SUCCESS,
+  FEED_CREATE_FAIL,
+  FEED_CREATE_RESET,
 } = require('../constants/feedConstants');
 
 export const feedListReducer = (
@@ -18,6 +22,21 @@ export const feedListReducer = (
       };
     case FEED_LIST_FAIL:
       return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const feedCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case FEED_CREATE_REQUEST:
+      return { loading: true };
+    case FEED_CREATE_SUCCESS:
+      return { loading: false, success: true, feed: action.payload };
+    case FEED_CREATE_FAIL:
+      return { loading: false, error: action.payload };
+    case FEED_CREATE_RESET:
+      return {};
     default:
       return state;
   }
