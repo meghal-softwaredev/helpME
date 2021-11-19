135350f16fb281d1f72c341358f525625f421b91
Umang Patel <umangpatel1991@gmail.com>
2021-11-19 18:49:47 -0500

    change term as user

--- server/models/feedModel.js
+++ server/models/feedModel.js
@@ -4,13 +4,14 @@ const feedSchema = new mongoose.Schema(
     title: { type: String, required: true },
     description: { type: String, required: true },
     verified_answer_id: { type: mongoose.Schema.Types.ObjectID, ref: 'FeedAnswer' },
-    user_id: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
-    category_id: { type: mongoose.Schema.Types.ObjectID, ref: 'Category' },
+    user: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
+    category: { type: mongoose.Schema.Types.ObjectID, ref: 'Category' },
     tags: [{ type: String, required: true }],
     answers: [{
       answer: { type: String, required: true },
-      user_id: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
-      upvotes: { type: String, required: true }
+      user: { type: mongoose.Schema.Types.ObjectID, ref: 'User' },
+      upvotes: { type: String, required: true },
+      posted_at: { type: Date, default: Date.now, required: true }
     }]
   },
   {
