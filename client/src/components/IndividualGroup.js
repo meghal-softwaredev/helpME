786cbb15a252475436ce4e722ff31d0844d0d17e
Meghal <meghal.softwaredev@gmail.com>
2021-11-19 14:13:32 -0500

    Update store and IndividualGroup component

--- client/src/components/IndividualGroup.js
+++ client/src/components/IndividualGroup.js
@@ -3,14 +3,20 @@ import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import LoadingBox from './LoadingBox';
 import MessageBox from './MessageBox';
+import { Grid, Button, Typography } from '@mui/material';
+import { getIndividualGroup } from '../actions/groupActions';
+import NewGroup from './NewGroup';
 
-function IndividualGroup() {
-  let groupId = useParams().id;
+function IndividualGroup(props) {
+  const { id } = useParams();
+
+  const individualGroupDetails = useSelector((state) => state.individualGroupDetails);
+  const { loading, error, group } = individualGroupDetails;
   
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(getIndividualGroup(groupId));
-  }, [dispatch, groupId]);
+    dispatch(getIndividualGroup(id));
+  }, [dispatch, id]);
 
   return (
     <div className="item-container">
@@ -19,19 +25,28 @@ function IndividualGroup() {
     ) : error ? (
       <MessageBox variant="danger">{error}</MessageBox>
     ) : (
+      <div>
+      <Button size="small" variant="contained" onClick={handleOpenNewGroup}>
+        Edit
+      </Button>
+      <NewGroup openNewGroup={openNewGroup} handleCloseNewGroup={handleCloseNewGroup} />
+      <Button size="small" variant="contained" onClick={handleOpenNewEvent}>
+        Delete
+      </Button>
       <Grid container sx={{ border: 1, my: 2, p: 2 }}>
       <Grid item xs={3} > 
-        <img src={group_url} width="150px" height="150px"/>
+        <img src={group.group_url} width="150px" height="150px"/>
       </Grid>
       <Grid item xs={9} sx={{ fontSize: 'h6.fontSize', fontWeight: 'medium', mb: 2 }}>
         <Typography component="h5" variant="h5">
-          <Link className="link" to={`/groups/${groupId}`}>{title}</Link>
+          <Link className="link" to={`/groups/${id}`}>{group.title}</Link>
         </Typography>
-        <Typography component="h6" variant="h6">{description}</Typography>
+        <Typography component="h6" variant="h6">{group.description}</Typography>
         <Button variant="contained">Join</Button>
       </Grid>
     </Grid>
-      )}
+    </div>
+    )} 
     </div>
   );
 }
