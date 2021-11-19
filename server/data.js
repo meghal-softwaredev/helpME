9a9a04b6f292a604d3437efcf0139445c612b4c3
Umang Patel <umangpatel1991@gmail.com>
2021-11-19 07:46:37 -0500

    modify feeds sample data

--- server/data.js
+++ server/data.js
@@ -56,7 +56,18 @@ const data = {
         Uncaught TypeError: Property 'CommentList' of object [object Object] is not a function`,
       user_id: "618dcf88205f076ec8f55d11",
       category_id: "6190afe0ee6fb3c345e5c6f9",
-      tags: [`React`]
+      tags: [`React`],
+      answers: [
+        {
+          answer: `There are two main issues going on here.
+        First, when React.renderComponent is called, CommentList hasn't been assigned, and is therefore still undefined. This is causing an error because CommentBox's render function refers to
+        <CommentList />
+        which jsx compiles to
+        CommentList(null)`,
+          user_id: "6191a1242faea3ef49478687",
+          upvotes: 10
+        }
+      ]
     },
     {
       title: `Why do my car cut off when I slow down or come to a complete stop?`,
@@ -67,36 +78,32 @@ const data = {
         No I took it to a shop but it was for something different and I thought that would make it stop cutting off`,
       user_id: "618dd0f544fed0b263668d6c",
       category_id: "6190afe0ee6fb3c345e5c6fa",
-      tags: [`2007`, `Honda`, `Accord`]
+      tags: [`2007`, `Honda`, `Accord`],
+      answers: [
+        {
+          answer: `I’m very sorry that you’re having this issue and together, we will get this figured out. I will need your help too because I can’t see, touch, hear or smell anything that’s going on with your vehicle. We may also require some specialized testing tools in order to assist us in diagnosing the problems due to the complexity of today’s vehicles. JA will offer a phone call to you at an additional fee, it’s your choice. Please be patient with my replies, I reply back in question order received. If I send you an attachment, I will tell you, BUT, you will NOT get it on a phone; you must be on a tablet or PC. Please let me know when you are satisfied with my answers too so we can close out your question
+        Okay, so the shop should scan the computer checking for current and pending codes as well as check your idle speed motor/throttle body.
+        They should also check for vacuum leaks too.
+        This data can help a lot finding that stalling issue.
+        Thanks again and all the best:)`,
+          user_id: "61919998bf9891d25a41d814",
+          upvotes: 50
+        }
+      ]
     },
     {
       title: `What to Eat With Hummus?`,
       description: `You’d be hard-pressed to find a spread (or is it a dip?) more universally loved than . The delicious blend of mashed chickpeas, tahini, oils, and spices is a welcome sight on any table. But, after a while,  gets boring. How many different foods can you even eat hummus? `,
       user_id: "618dee2b0b54ac2ca484ba21",
       category_id: "6190afe0ee6fb3c345e5c6fb",
-      tags: [`Hummus`]
-    }
-  ],
-  feedAnswers: [
-    {
-      answer: `There are two main issues going on here.
-        First, when React.renderComponent is called, CommentList hasn't been assigned, and is therefore still undefined. This is causing an error because CommentBox's render function refers to
-        <CommentList />
-        which jsx compiles to
-        CommentList(null)`,
-      upvotes: 10
-    },
-    {
-      answer: `I’m very sorry that you’re having this issue and together, we will get this figured out. I will need your help too because I can’t see, touch, hear or smell anything that’s going on with your vehicle. We may also require some specialized testing tools in order to assist us in diagnosing the problems due to the complexity of today’s vehicles. JA will offer a phone call to you at an additional fee, it’s your choice. Please be patient with my replies, I reply back in question order received. If I send you an attachment, I will tell you, BUT, you will NOT get it on a phone; you must be on a tablet or PC. Please let me know when you are satisfied with my answers too so we can close out your question
-        Okay, so the shop should scan the computer checking for current and pending codes as well as check your idle speed motor/throttle body.
-        They should also check for vacuum leaks too.
-        This data can help a lot finding that stalling issue.
-        Thanks again and all the best:)`,
-      user_id: 6,
-    },
-    {
-      answer: `You can’t go wrong with a classic. Whether it’s white or wheat, hard or soft, pita bread and hummus are always a perfect pairing.`,
-      upvotes: 20
+      tags: [`Hummus`],
+      answers: [
+        {
+          answer: `You can’t go wrong with a classic. Whether it’s white or wheat, hard or soft, pita bread and hummus are always a perfect pairing.`,
+          user_id: "61919a3ebf9891d25a41d817",
+          upvotes: 20
+        }
+      ]
     }
   ],
   groups: [
