3def79aa8adff737ff94e982a87bb427dd1c2788
Umang Patel <umangpatel1991@gmail.com>
2021-11-19 18:51:31 -0500

    apply dark theme to component

--- client/src/components/IndividualFeed.js
+++ client/src/components/IndividualFeed.js
@@ -1,7 +1,10 @@
 import { useEffect, useState } from 'react';
+import { ThemeProvider } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link, useParams } from 'react-router-dom';
 import { Chip, Box, Divider } from '@mui/material';
+
+import { darkTheme } from "../mui/themes";
 import { getIndividualFeed } from '../actions/feedActions';
 import LoadingBox from './LoadingBox';
 import MessageBox from './MessageBox';
@@ -22,35 +25,36 @@ function IndividualFeed() {
   }, [dispatch, feedId]);
 
   return (
-    <div className="feed-item-container">
-      {loading ? (
-        <LoadingBox></LoadingBox>
-      ) : error ? (
-        <MessageBox variant="danger">{error}</MessageBox>
-      ) : (
-      <Box sx={{ border: 1, my: 2, p: 2 }}>
-        <Box sx={{ fontSize: 'h6.fontSize', fontWeight: 'medium', mb: 2 }}>
-          {feed.title}
-        </Box>
-        {feed.tags.map(tag => (
-          <Chip key={tag} sx={{ mr: 1 }} label={tag} color="primary" />
-        ))}
-        <p>{feed.description}</p>
-        <br />
-        <Divider light={true}>
-          <Chip label="ANSWERS" sx={{ backgroundColor: '#fff' }} />
-        </Divider>
-              {feed.answers && feed.answers.map(ans => (
-                <div key={ans._id}>
-                  <p>{ans.answer}</p>
-                  <p>{ans.answer}</p>
-                </div>
+    <ThemeProvider theme={darkTheme}>
+      <div className="feed-item-container">
+        {loading ? (
+          <LoadingBox></LoadingBox>
+        ) : error ? (
+          <MessageBox variant="danger">{error}</MessageBox>
+        ) : (
+          <Box sx={{ border: 1, my: 2, p: 2 }}>
+            <Box sx={{ fontSize: 'h6.fontSize', fontWeight: 'medium', mb: 2 }}>
+              {feed.title}
+            </Box>
+            {feed.tags.map(tag => (
+              <Chip key={tag} sx={{ mr: 1 }} label={tag} color="primary" />
+            ))}
+            <p>{feed.description}</p>
+            <br />
+            <Divider light={true}>
+              <Chip label="ANSWERS"/>
+            </Divider>
+            {feed.answers && feed.answers.map(ans => (
+              <div key={ans._id}>
+                <p>{ans.answer}</p>
+              </div>
 
-              ))}
-        
-      </Box>
-      )}
-    </div>
+            ))}
+
+          </Box>
+        )}
+      </div>
+    </ThemeProvider>
   );
 }
 
