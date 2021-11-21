255ef01103858a95737e35bfc5f87ca604a91b0c
Meghal <meghal.softwaredev@gmail.com>
2021-11-20 22:49:58 -0500

    Add reducer to create event form

--- client/src/reducers/eventReducers.js
+++ client/src/reducers/eventReducers.js
@@ -1,7 +1,10 @@
 import {
   EVENT_LIST_REQUEST,
   EVENT_LIST_SUCCESS,
-  EVENT_LIST_FAIL
+  EVENT_LIST_FAIL,
+  EVENT_CREATE_REQUEST,
+  EVENT_CREATE_SUCCESS,
+  EVENT_CREATE_FAIL,
 } from '../constants/eventConstants';
 
 export const eventListReducer = (state = { loading: true, events: [] }, action) => {
@@ -11,6 +14,19 @@ export const eventListReducer = (state = { loading: true, events: [] }, action) 
     case EVENT_LIST_SUCCESS:
       return { loading: false, events: action.payload.events };
     case EVENT_LIST_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const eventCreateReducer = (state = {}, action) => {
+  switch (action.type) {
+    case EVENT_CREATE_REQUEST:
+      return { loading: true };
+    case EVENT_CREATE_SUCCESS:
+      return { loading: false, success: true, event: action.payload };
+    case EVENT_CREATE_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
