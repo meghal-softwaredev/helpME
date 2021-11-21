648eb959ba114079d39a846b1fd8eb0d5d7afa31
Meghal <meghal.softwaredev@gmail.com>
2021-11-21 00:29:55 -0500

    Add reducer to show individual event

--- client/src/reducers/eventReducers.js
+++ client/src/reducers/eventReducers.js
@@ -5,6 +5,9 @@ import {
   EVENT_CREATE_REQUEST,
   EVENT_CREATE_SUCCESS,
   EVENT_CREATE_FAIL,
+  INDIVIDUAL_EVENT_DETAILS_REQUEST,
+  INDIVIDUAL_EVENT_DETAILS_SUCCESS,
+  INDIVIDUAL_EVENT_DETAILS_FAIL,
 } from '../constants/eventConstants';
 
 export const eventListReducer = (state = { loading: true, events: [] }, action) => {
@@ -27,6 +30,19 @@ export const eventCreateReducer = (state = {}, action) => {
     case EVENT_CREATE_SUCCESS:
       return { loading: false, success: true, event: action.payload };
     case EVENT_CREATE_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const individualEventDetailsReducer = (state = { loading: true }, action) => {
+  switch (action.type) {
+    case INDIVIDUAL_EVENT_DETAILS_REQUEST:
+      return { loading: true };
+    case INDIVIDUAL_EVENT_DETAILS_SUCCESS:
+      return { loading: false, event: action.payload };
+    case INDIVIDUAL_EVENT_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     default:
       return state;
