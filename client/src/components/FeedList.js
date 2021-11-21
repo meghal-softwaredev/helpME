d456a762761716aabc5a9ae1b4b23932601a1f77
Umang Patel <umangpatel1991@gmail.com>
2021-11-21 00:03:35 -0500

    add conditional render to post question

--- client/src/components/FeedList.js
+++ client/src/components/FeedList.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import { Button } from '@mui/material';
 import { listFeeds } from '../actions/feedActions';
 import FeedListItem from './FeedListItem';
@@ -9,15 +9,18 @@ import NewFeed from './NewFeed';
 function FeedList(props) {
   const [openNewFeed, setOpenNewFeed] = React.useState(false);
 
+  const navigate = useNavigate();
+
   const feedList = useSelector((state) => state.feedList);
   const { loading, error, feeds } = feedList;
 
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
+
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch( listFeeds() );
-  }, [dispatch, openNewFeed ]);
+  }, [dispatch, openNewFeed, navigate, props.history ]);
 
   const handleOpenNewFeed = () => {
     setOpenNewFeed(true);
@@ -29,12 +32,14 @@ function FeedList(props) {
 
   return (
     <div>
-      <div>
-        <Button size="large" variant="contained" onClick={handleOpenNewFeed}>
-          Post Question
-        </Button>
-        <NewFeed activity="new" openNewFeed={openNewFeed} handleCloseNewFeed={handleCloseNewFeed} />
-      </div>
+      {userInfo && (
+        <div>
+          <Button size="large" variant="contained" onClick={handleOpenNewFeed}>
+            Post Question
+          </Button>
+          <NewFeed activity="new" openNewFeed={openNewFeed} handleCloseNewFeed={handleCloseNewFeed} />
+        </div>
+      )}
       {loading ? (
         <span>Loading</span>
       ) : error ? (
