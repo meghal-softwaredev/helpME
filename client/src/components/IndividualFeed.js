a082b988f161c141b721bca7c8262a669ca30232
Umang Patel <umangpatel1991@gmail.com>
2021-11-21 00:07:53 -0500

    implement delete feed functionality

--- client/src/components/IndividualFeed.js
+++ client/src/components/IndividualFeed.js
@@ -1,23 +1,27 @@
 import React, { useEffect, useState } from 'react';
 import { ThemeProvider } from '@mui/material/styles';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link, useParams } from 'react-router-dom';
+import { Link, useParams, useNavigate } from 'react-router-dom';
 import { Chip, Box, Divider, Container, Typography, Grid, TextField, Button, Card, CardContent, CardActions, IconButton } from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import DeleteIcon from '@mui/icons-material/Delete';
 import { darkTheme } from "../mui/themes";
-import { getIndividualFeed, getFeedAnswers, saveAnswer } from '../actions/feedActions';
+import { getIndividualFeed, getFeedAnswers, saveAnswer, deleteFeed } from '../actions/feedActions';
 import LoadingBox from './LoadingBox';
 import MessageBox from './MessageBox';
 import { borderColor } from '@mui/system';
 import NewFeed from './NewFeed';
+import DeleteConfirmDialog from './DeleteConfirmDialog';
 
 function IndividualFeed() {
 
   const [newAnswer, setNewAnswer] = useState("");
-  const [openNewFeed, setOpenNewFeed] = React.useState(false);
+
+  const [openDialog, setOpenDialog] = React.useState(false);
+  const [currentDialog, setcurrentDialog] = React.useState("");
 
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   let feedId = useParams().id;
 
   const individualFeedDetails = useSelector((state) => state.individualFeedDetails);
@@ -30,22 +34,31 @@ function IndividualFeed() {
 
   useEffect(() => {
     dispatch(getIndividualFeed(feedId));
-  }, [dispatch, feedId]);
+  }, [dispatch, feedId, openDialog]);
 
   useEffect(() => {
     dispatch(getFeedAnswers(feedId));
-  }, []);
+  }, [openDialog]);
 
   function handleNewAnswerChange(e) {
     setNewAnswer(e.target.value);
   };
 
-  const handleOpenNewFeed = () => {
-    setOpenNewFeed(true);
+  const handleOpenDialog = (currentTask) => {
+    setcurrentDialog(currentTask);
+    setOpenDialog(true);
   };
 
-  const handleCloseNewFeed = () => {
-    setOpenNewFeed(false);
+  const handleCloseDialog = () => {
+    setOpenDialog(false);
+  };
+
+  const handleConfirmDelete = (currentTask) => {
+    dispatch(
+      currentTask === "DeleteFeed" && deleteFeed(feedId)
+    );
+    setOpenDialog(false);
+    navigate('/feeds');
   };
 
   const handleSubmit = (event) => {
@@ -75,17 +88,33 @@ function IndividualFeed() {
                 <p>{feed.description}</p>
                 <p>Posted by: {feed.user && feed.user.name}</p>
                 <p>Posted on: {feed.createdAt}</p>
-                {feed.user && feed.user._id === userInfo._id && (
+                {userInfo && feed.user && feed.user._id === userInfo._id && (
                   <CardActions>
-                    <IconButton color="warning" aria-label="edit answer" component="span" onClick={handleOpenNewFeed}>
+                    <IconButton color="warning" aria-label="edit feed" component="span" onClick={() => handleOpenDialog("EditFeed")}>
                       <EditIcon />
                     </IconButton>
-                    <IconButton sx={{ color: "#df7373" }} aria-label="edit answer" component="span">
+                    <IconButton sx={{ color: "#df7373" }} aria-label="delete feed" component="span" onClick={() => handleOpenDialog("DeleteFeed")}>
                       <DeleteIcon />
                     </IconButton>
                   </CardActions>
                 )}
-                <NewFeed activity="edit" feed={feed} openNewFeed={openNewFeed} handleCloseNewFeed={handleCloseNewFeed} />
+                {currentDialog === "EditFeed" && (
+                  <NewFeed
+                    activity="EditFeed"
+                    feed={feed}
+                    openNewFeed={openDialog}
+                    handleCloseNewFeed={handleCloseDialog} />
+                )}
+                {currentDialog === "DeleteFeed" && (
+                  <DeleteConfirmDialog
+                    activity="DeleteFeed"
+                    title="Delete Feed?"
+                    openDeleteDialog={openDialog} 
+                    handleCloseDeleteDialog={handleCloseDialog}
+                    handleConfirmDelete={handleConfirmDelete} >
+                      Are you sure you want to delete this feed?
+                  </DeleteConfirmDialog>
+                )}
             <br />
             <Divider light={true}>
               <Chip label="ANSWERS"/>
@@ -102,12 +131,12 @@ function IndividualFeed() {
                         </Typography>
                         
                       </CardContent>
-                      {userInfo._id === ans.user._id && (
+                      {userInfo && userInfo._id === ans.user._id && (
                         <CardActions>
                           <IconButton color="warning" aria-label="edit answer" component="span">
                             <EditIcon />
                           </IconButton>
-                          <IconButton sx={{ color: "#df7373" }} aria-label="edit answer" component="span">
+                          <IconButton sx={{ color: "#df7373" }} aria-label="delete answer" component="span">
                             <DeleteIcon />
                           </IconButton>
                         </CardActions>
@@ -116,30 +145,33 @@ function IndividualFeed() {
                     </Card>
                   </Box>
             ))}
-                <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
-                  <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                      <TextField
-                        required
-                        fullWidth
-                        id="answer"
-                        label="Answer"
-                        placeholder="Answer"
-                        multiline
-                        rows={4}
-                        value={newAnswer}
-                        onChange={handleNewAnswerChange}
-                      />
-                    </Grid>
+            {userInfo && (
+              <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <TextField
+                      required
+                      fullWidth
+                      id="answer"
+                      label="Answer"
+                      placeholder="Answer"
+                      multiline
+                      rows={4}
+                      value={newAnswer}
+                      onChange={handleNewAnswerChange}
+                    />
                   </Grid>
-                  <Button
-                    type="submit"
-                    variant="contained"
-                    sx={{ mt: 3, mb: 2 }}
-                  >
-                    Post Answer
-                  </Button>
-                </Box>
+                </Grid>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  sx={{ mt: 3, mb: 2 }}
+                >
+                  Post Answer
+                </Button>
+              </Box>
+            )}
+                
           </Box>
         )}
       </div>
