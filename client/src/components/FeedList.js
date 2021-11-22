7a8f563bdb82f93190ae1ca3d7c4a5f6311c784f
Umang Patel <umangpatel1991@gmail.com>
2021-11-22 10:56:50 -0500

    initial raw navbar design

--- client/src/components/FeedList.js
+++ client/src/components/FeedList.js
@@ -47,64 +47,66 @@ function FeedList(props) {
 
   return (
     <ThemeProvider theme={darkTheme}>
-      <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", border: '1px solid #0077b6', my: 2, px: 2 }}>
-        <Box
-          component="form"
-          noValidate
-          autoComplete="off"
-        >
-          <TextField 
-            id="outlined-basic"
-            variant="outlined"
-            size="small"
-            value={keywordValue}
-            onChange={handleKeywordValueChange}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }} />
-        </Box>
-        <Box>
-          <FormControl sx={{ m: 1, minWidth: 120 }}>
-            <Select
-              labelId="controlled-open-sort-select-label"
-              id="controlled-sort-select"
+      <Box sx={{flex:1, display:"flex", flexDirection:"column", justifyContent:"center"}}>
+        <Box sx={{ display: "flex", justifyContent: "space-between", alignItems: "center", border: '1px solid #0077b6', my: 2, px: 2 }}>
+          <Box
+            component="form"
+            noValidate
+            autoComplete="off"
+          >
+            <TextField 
+              id="outlined-basic"
+              variant="outlined"
               size="small"
-              displayEmpty
-              inputProps={{ 'aria-label': 'Without label' }}
-              open={openFeedSort}
-              onClose={handleSortSelectClose}
-              onOpen={handleSortSelectOpen}
-              value={sortValue}
-              onChange={handleSortValueChange}
-            >
-              <MenuItem value="">
-                <em>Sort</em>
-              </MenuItem>
-              <MenuItem value={"latest"}>Latest First</MenuItem>
-              <MenuItem value={"oldest"}>Oldest First</MenuItem>
-            </Select>
-          </FormControl>
+              value={keywordValue}
+              onChange={handleKeywordValueChange}
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }} />
+          </Box>
+          <Box>
+            <FormControl sx={{ m: 1, minWidth: 120 }}>
+              <Select
+                labelId="controlled-open-sort-select-label"
+                id="controlled-sort-select"
+                size="small"
+                displayEmpty
+                inputProps={{ 'aria-label': 'Without label' }}
+                open={openFeedSort}
+                onClose={handleSortSelectClose}
+                onOpen={handleSortSelectOpen}
+                value={sortValue}
+                onChange={handleSortValueChange}
+              >
+                <MenuItem value="">
+                  <em>Sort</em>
+                </MenuItem>
+                <MenuItem value={"latest"}>Latest First</MenuItem>
+                <MenuItem value={"oldest"}>Oldest First</MenuItem>
+              </Select>
+            </FormControl>
+          </Box>
         </Box>
-      </Box>
-      <Box sx={{ border: '1px solid #0077b6', my: 2, px: 2 }}>
-        {loading ? (
-          <span>Loading</span>
-        ) : error ? (
-          <span>Error: {error}</span>
-        ) : (
-          <>
-            {feeds.map((feed) => (
-              <React.Fragment key= { feed._id }>
-                <FeedListItem feed={feed} />
-                <Divider />
-              </React.Fragment>
-            ))}
-          </>
-        )}
+        <Box sx={{ border: '1px solid #0077b6', my: 2, px: 2 }}>
+          {loading ? (
+            <span>Loading</span>
+          ) : error ? (
+            <span>Error: {error}</span>
+          ) : (
+            <>
+              {feeds.map((feed) => (
+                <React.Fragment key= { feed._id }>
+                  <FeedListItem feed={feed} />
+                  <Divider />
+                </React.Fragment>
+              ))}
+            </>
+          )}
+        </Box>
       </Box>
     </ThemeProvider>
   );
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,31 @@
+import * as React from 'react';
 import { Link } from "react-router-dom"
 import "../styles/components/Header.scss";
 import { useDispatch, useSelector } from 'react-redux';
 import { signout } from "../actions/userActions";
 
+import { ThemeProvider } from '@mui/material/styles';
+
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import Switch from '@mui/material/Switch';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormGroup from '@mui/material/FormGroup';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import { CssBaseline } from '@mui/material';
+
+import { darkTheme } from "../mui/themes";
+
 function Header() {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
   const userSignin = useSelector((state) => state.userSignin);
   const { userInfo } = userSignin;
 
@@ -12,48 +34,69 @@ function Header() {
     dispatch(signout());
   };
 
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <header className="row">
-      <div className="logo">
-        <Link className="brand link" to="/feeds">
-          helpME
-        </Link>
-      </div>
-      {userInfo ? (
-        <nav>
-          <Link className="link" to="/getHelp">
-            Get Help
-          </Link>
-        
-          <div className="dropdown">
-            <Link className="link" to="#"> Name
-              {userInfo.name} <i className="fa fa-caret-down"></i>{' '}
-            </Link>
-            <ul className="dropdown-content reset-ul">
-              <li>
-                <Link className="link" to="/profile">User Profile</Link>
-              </li>
-              <li>
-                <Link className="link" to="#signout" onClick={signoutHandler}>
-                  Sign Out
-                </Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-          ) : ( 
-        <nav>
-            <ul className="reset-ul">
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/signin">Sign In</Link>
-            </li>
-          </ul>
-        </nav>
-        )}  
-    </header>
+    <ThemeProvider theme={darkTheme}>
+      {/* <Box className="header" sx={{ flexGrow: 1 }}> */}
+      <CssBaseline />
+      <AppBar className="header" position="sticky" sx={{justifyContent:"center"}}>
+          <Toolbar>
+            <Typography variant="h4" component="div">
+              helpME
+            </Typography>
+            <Box sx={{ ml:5, flexGrow: 1 }}>
+            <Link className="link nav-link get-help-btn" to="#">Get Help</Link>
+            </Box>
+            {userInfo ? (
+              <div>
+                <IconButton
+                  aria-label="account of current user"
+                  aria-controls="menu-appbar"
+                  aria-haspopup="true"
+                  onClick={handleMenu}
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton>
+                <Menu
+                  id="menu-appbar"
+                  anchorEl={anchorEl}
+                  anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  keepMounted
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                  open={Boolean(anchorEl)}
+                  onClose={handleClose}
+                >
+                  <MenuItem onClick={handleClose}>Profile</MenuItem>
+                  <MenuItem onClick={signoutHandler}>Logout</MenuItem>
+                </Menu>
+              </div>
+            ) : (
+              <div>
+                <Link className="link nav-link" to="/events">Events</Link>
+                <Link className="link nav-link" to="/feeds">Feed</Link>
+                <Link className="link nav-link" to="#">About Us</Link>
+                <Link className="link nav-link" to="/register">Register</Link>
+                <Link className="link nav-link" to="/signin">Login</Link>
+              </div>
+            )}
+          </Toolbar>
+        </AppBar>
+      {/* </Box> */}
+    </ThemeProvider>
   )
 }
 
