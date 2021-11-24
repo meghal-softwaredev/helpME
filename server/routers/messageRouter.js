f2271cf8ce8042f0cbcd7b27061a6f65abe0f4bc
Meghal <meghal.softwaredev@gmail.com>
2021-11-23 23:53:06 -0500

    Add messageRouter for chat

---
+++ server/routers/messageRouter.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import expressAsyncHandler from 'express-async-handler';
+import Message from '../models/messageModel.js';
+import { isAuth } from '../helpers/utils.js';
+
+const messageRouter = express.Router();
+
+messageRouter.get(
+  '/',
+  expressAsyncHandler(async (req, res) => {
+    const messages = await Message.find({});
+    console.log("hi", messages);
+    res.send({ messages });
+  })
+);
+
+messageRouter.post(
+  '/new',
+  expressAsyncHandler(async (req, res) => {
+    console.log("body", req.body);
+    const message = new Message({
+      message_text: req.body.message_text,
+      user_id: req.body.user_id
+    });
+    const createdMessage = await message.save();
+    res.send({ messageInfo: 'Message Created', message: createdMessage });
+  })
+);
+
+export default messageRouter;
