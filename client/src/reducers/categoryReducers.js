f86ef2954302dff6565861babc23d9246909e298
jessonziegler <jziegleraudio@gmail.com>
2021-11-24 12:07:57 -0500

    add category reducer

--- client/src/reducers/categoryReducers.js
+++ client/src/reducers/categoryReducers.js
@@ -1,8 +1,12 @@
 const {
   CATEGORY_LIST_REQUEST,
   CATEGORY_LIST_SUCCESS,
-  CATEGORY_LIST_FAIL
-} = require('../constants/categoryConstants');
+  CATEGORY_LIST_FAIL,
+  PREFERRED_CATEGORIES_SAVE_REQUEST,
+  PREFERRED_CATEGORIES_SAVE_SUCCESS,
+  PREFERRED_CATEGORIES_SAVE_FAIL,
+  PREFERRED_CATEGORIES_SAVE_RESET,
+} = require("../constants/categoryConstants");
 
 export const categoryListReducer = (
   state = { loading: true, categories: [] },
@@ -14,10 +18,29 @@ export const categoryListReducer = (
     case CATEGORY_LIST_SUCCESS:
       return {
         loading: false,
-        categories: action.payload.categories
+        categories: action.payload.categories,
       };
     case CATEGORY_LIST_FAIL:
       return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export const preferredCategoriesSaveReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PREFERRED_CATEGORIES_SAVE_REQUEST:
+      return { loading: true };
+    case PREFERRED_CATEGORIES_SAVE_SUCCESS:
+      return {
+        loading: false,
+        success: true,
+        preferred_categories: action.payload,
+      };
+    case PREFERRED_CATEGORIES_SAVE_FAIL:
+      return { loading: false, error: action.payload };
+    case PREFERRED_CATEGORIES_SAVE_RESET:
+      return {};
     default:
       return state;
   }
