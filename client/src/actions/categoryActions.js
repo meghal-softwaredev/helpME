8fe88b27c376db55e91268874973fa2d16d37445
jessonziegler <jziegleraudio@gmail.com>
2021-11-24 12:05:05 -0500

    axios call to get categories

--- client/src/actions/categoryActions.js
+++ client/src/actions/categoryActions.js
@@ -1,20 +1,52 @@
-import Axios from 'axios';
+import Axios from "axios";
 import {
   CATEGORY_LIST_FAIL,
   CATEGORY_LIST_REQUEST,
-  CATEGORY_LIST_SUCCESS
-} from '../constants/categoryConstants';
+  CATEGORY_LIST_SUCCESS,
+  PREFERRED_CATEGORIES_SAVE_REQUEST,
+  PREFERRED_CATEGORIES_SAVE_SUCCESS,
+  PREFERRED_CATEGORIES_SAVE_FAIL,
+  PREFERRED_CATEGORIES_SAVE_RESET,
+} from "../constants/categoryConstants";
 
 export const listCategories = () => async (dispatch) => {
   dispatch({
     type: CATEGORY_LIST_REQUEST,
   });
   try {
-    const { data } = await Axios.get(
-      `/api/categories`
-    );
+    const { data } = await Axios.get(`/api/categories`);
     dispatch({ type: CATEGORY_LIST_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: CATEGORY_LIST_FAIL, payload: error.message });
   }
 };
+
+export const savePrefferedCategories =
+  (prefferedCategories) => async (dispatch, getState) => {
+    dispatch({ type: PREFERRED_CATEGORIES_SAVE_REQUEST });
+    const {
+      userSignin: { userInfo },
+    } = getState();
+    try {
+      const { data } = await Axios.post(
+        "/api/profiles/preferred_categories/new",
+        {
+          prefferedCategories,
+          user_id: userInfo._id,
+        },
+        {
+          headers: { Authorization: `Bearer ${userInfo.token}` },
+        }
+      );
+      dispatch({
+        type: PREFERRED_CATEGORIES_SAVE_SUCCESS,
+        payload: data.preferred_categories,
+      });
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      dispatch({ type: PREFERRED_CATEGORIES_SAVE_FAIL, payload: message });
+    }
+  };
