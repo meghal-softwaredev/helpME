b9507e9eacbd8d0156b10f858672cfb4569c2c62
jessonziegler <jziegleraudio@gmail.com>
2021-11-24 12:11:46 -0500

    fixing colours

--- client/src/components/FeedListItem.js
+++ client/src/components/FeedListItem.js
@@ -1,31 +1,59 @@
-import { Chip, Box, Typography } from '@mui/material';
-import { Link } from 'react-router-dom';
-import moment from 'moment';
+import { Chip, Box, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+import moment from "moment";
 import "../styles/components/FeedListItem.scss";
 function FeedListItem(props) {
   return (
     <div className="feed-item-container">
-      <Box sx={{ my: 1, p: 2, display: 'flex', gap: '10px', alignItems: "center" }}>
-        <Box sx={{ flex: '1' }}>
-          <Box sx={{ fontSize: 'h5.fontSize', flex: '1', fontWeight: 'medium', mb: 1 }}>
-            <Link className="link" to={`/feeds/${props.feed._id}`}>{props.feed.title}</Link>
+      <Box
+        sx={{ my: 1, p: 2, display: "flex", gap: "10px", alignItems: "center" }}
+      >
+        <Box sx={{ flex: "1" }}>
+          <Box
+            sx={{
+              fontSize: "h5.fontSize",
+              flex: "1",
+              fontWeight: "medium",
+              mb: 1,
+            }}
+          >
+            <Link className="link" to={`/feeds/${props.feed._id}`}>
+              {props.feed.title}
+            </Link>
           </Box>
-          <Box sx={{ display: 'flex', gap: '10px', flexDirection: 'column' }}>
+          <Box sx={{ display: "flex", gap: "10px", flexDirection: "column" }}>
             <Box>
-              {props.feed.tags.map(tag => (
-                <Chip key={tag} sx={{ mr: 1 }} label={tag} color="primary" variant="outlined" size="small" />
+              {props.feed.tags.map((tag) => (
+                <Chip
+                  key={tag}
+                  sx={{ mr: 1 }}
+                  label={tag}
+                  color="secondary"
+                  variant="outlined"
+                  size="small"
+                />
               ))}
             </Box>
-            <Typography sx={{ fontSize: '13px' }}>{moment(props.feed.createdAt).format("MMMM Do, YYYY")}</Typography>
+            <Typography sx={{ fontSize: "13px" }}>
+              {moment(props.feed.createdAt).format("MMMM Do, YYYY")}
+            </Typography>
           </Box>
         </Box>
-        <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <Typography sx={{ fontSize: 'h6.fontSize', fontWeight: 'bold' }}>{props.feed.answers.length}</Typography>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          <Typography sx={{ fontSize: "h6.fontSize", fontWeight: "bold" }}>
+            {props.feed.answers.length}
+          </Typography>
           <Typography>Answers</Typography>
         </Box>
       </Box>
     </div>
-  )
+  );
 }
 
-export default FeedListItem
+export default FeedListItem;
--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import expressAsyncHandler from 'express-async-handler';
-import bcrypt from 'bcryptjs';
-import User from '../models/userModel.js';
-import data from '../data.js';
-import { generateToken } from '../helpers/utils.js';
+import express from "express";
+import expressAsyncHandler from "express-async-handler";
+import bcrypt from "bcryptjs";
+import User from "../models/userModel.js";
+import data from "../data.js";
+import Profile from "../models/profileModel.js";
+import { generateToken } from "../helpers/utils.js";
 
 const userRouter = express.Router();
 
 userRouter.get(
-  '/seed',
+  "/seed",
   expressAsyncHandler(async (req, res) => {
     await User.remove({});
     const createdUsers = await User.insertMany(data.users);
@@ -17,26 +18,26 @@ userRouter.get(
 );
 
 userRouter.post(
-  '/register',
+  "/register",
   expressAsyncHandler(async (req, res) => {
     const user = new User({
       name: req.body.name,
       email: req.body.email,
-      password: bcrypt.hashSync(req.body.password, 8)
+      password: bcrypt.hashSync(req.body.password, 8),
     });
- 
+
     const createdUser = await user.save();
     res.send({
       _id: createdUser._id,
       name: createdUser.name,
       email: createdUser.email,
-      token: generateToken(createdUser)
+      token: generateToken(createdUser),
     });
   })
 );
 
 userRouter.post(
-  '/signin',
+  "/signin",
   expressAsyncHandler(async (req, res) => {
     const user = await User.findOne({ email: req.body.email });
     if (user) {
@@ -45,23 +46,23 @@ userRouter.post(
           _id: user._id,
           name: user.name,
           email: user.email,
-          token: generateToken(user)
+          token: generateToken(user),
         });
         return;
       }
     }
-    res.status(401).send({ message: 'Invalid email or password' });
+    res.status(401).send({ message: "Invalid email or password" });
   })
 );
 
 userRouter.get(
-  '/:id',
+  "/:id",
   expressAsyncHandler(async (req, res) => {
     const user = await User.findById(req.params.id);
     if (user) {
       res.send(user);
     } else {
-      res.status(404).send({ message: 'User Not Found' });
+      res.status(404).send({ message: "User Not Found" });
     }
   })
 );
