f3076425833d84f63616425fe61c8313d38f479d
jessonziegler <jziegleraudio@gmail.com>
2021-11-24 12:00:06 -0500

    add category routes

--- server/models/categoryModel.js
+++ server/models/categoryModel.js
@@ -1,20 +1,19 @@
-import mongoose from 'mongoose';
-const categorySchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    img_url: { type: String },
-    createdAt: {
-      type: Date,
-      required: true,
-      default: Date.now
-    },
-    updatedAt: {
-      type: Date,
-      required: true,
-      default: Date.now
-    }
-  }
-);
+import mongoose from "mongoose";
+const categorySchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  description: { type: String, required: true },
+  img_url: { type: String },
+  createdAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    required: true,
+    default: Date.now,
+  },
+});
 
-const Category = mongoose.model('Category', categorySchema);
+const Category = mongoose.model("Category", categorySchema);
 export default Category;
