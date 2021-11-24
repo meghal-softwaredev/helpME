02716403c7784800f1d2e9594660ac1415a84459
Meghal <meghal.softwaredev@gmail.com>
2021-11-23 23:49:09 -0500

    Add ChatMessageList component for list of messages

---
+++ client/src/components/ChatMessageList.js
@@ -0,0 +1,16 @@
+import ScrollToBottom from "react-scroll-to-bottom";
+
+import ChatMessageListItem from "./ChatMessageListItem";
+
+// We use react-scroll-to-bottom here. It auto scrolls to bottom when height of messages exceeds the height of the container.
+const ChatMessageList = (props) => {
+  return (
+    <ScrollToBottom className="all-messages">
+      {props.messages.map((message) => (
+        <ChatMessageListItem key={message._id} message={message} />
+      ))}
+    </ScrollToBottom>
+  );
+};
+
+export default ChatMessageList;
