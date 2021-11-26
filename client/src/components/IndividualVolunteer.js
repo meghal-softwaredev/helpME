e5c704bc9560ecd068773745f466f1a35f24002b
Umang Patel <umangpatel1991@gmail.com>
2021-11-26 14:21:46 -0500

    show volunteer details info

---
+++ client/src/components/IndividualVolunteer.js
@@ -0,0 +1,124 @@
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+import { Box, IconButton, Container, Typography, Divider, Chip, Button } from "@mui/material";
+import { styled } from '@mui/material/styles';
+import Badge from '@mui/material/Badge';
+import Avatar from '@mui/material/Avatar';
+import EditIcon from '@mui/icons-material/Edit';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import TwitterIcon from '@mui/icons-material/Twitter';
+
+import "../styles/components/Profile.scss";
+
+import {
+  showProfileDetails,
+} from "../actions/profileActions";
+
+function IndividualVolunteer(props) {
+  const navigate = useNavigate();
+
+  const sections = {
+    "basic_info": {
+      title: "Intro"
+    },
+    "bio_details": {
+      title: "Bio"
+    },
+    "profile_links": {
+      title: "Profile Links"
+    },
+    "skills": {
+      title: "Skills"
+    },
+    "volunteer_status": {
+      title: "Volunteer Status"
+    }
+  };
+
+  const profileDetailsList = useSelector((state) => state.profileDetailsList);
+  const { loading, error, profileDetails } = profileDetailsList;
+
+  const userSignin = useSelector((state) => state.userSignin);
+  const { userInfo } = userSignin;
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+      props.currentVolunteer && dispatch(showProfileDetails(props.currentVolunteer));
+  }, [dispatch, props.currentVolunteer]);
+
+  const handleConnectVolunteer = (volunteer_id) => {
+    //Logic to call chat component
+  }
+
+  if (!props.currentVolunteer) {
+    return (
+      <div className="profile-container">
+        <p>Select Volunteer to show volunteer details</p>
+      </div>
+    );
+  }
+  return (
+        <div className="profile-container">
+          <div className="profile-container-header" />
+          <Box className="section profile-picture-container" >
+            <Avatar
+              //alt={profileDetails && profileDetails.user && profileDetails.user.name}
+              src={`${process.env.PUBLIC_URL}/images/avatar/profile-avatar.jpeg`}
+              sx={{ width: 200, height: 200 }} />
+          </Box>
+          <div className="section basic-info-container">
+            <p className="section-title">{profileDetails && profileDetails.user && profileDetails.user.name}</p>
+          </div>
+          <Divider sx={{ mx: 2 }} />
+          <div className="section bio-details-container">
+            <div>
+              <p className="section-title">Bio</p>
+              <p>{profileDetails && profileDetails.bio}</p>
+            </div>
+          </div>
+          <Divider sx={{ mx: 2 }} />
+          <div className="section profile-links-container">
+            <div>
+              <p className="section-title">Profile Links</p>
+              <Box className="profile-link-row"><GitHubIcon /> {profileDetails && profileDetails.github_url}</Box>
+              <Box className="profile-link-row"><LinkedInIcon /> {profileDetails && profileDetails.linkedin_url}</Box>
+              <Box className="profile-link-row"><FacebookIcon /> {profileDetails && profileDetails.facebook_url}</Box>
+              <Box className="profile-link-row"><InstagramIcon /> {profileDetails && profileDetails.instagram_url}</Box>
+              <Box className="profile-link-row"><TwitterIcon /> {profileDetails && profileDetails.twitter_url}</Box>
+            </div>
+          </div>
+          <Divider sx={{ mx: 2 }} />
+          <div className="section skills-container">
+            <div>
+              <p className="section-title">Skills</p>
+              <Box>
+                {profileDetails && profileDetails.skills && profileDetails.skills.map((skill) => (
+                  <Chip
+                    key={skill}
+                    sx={{ mr: 1 }}
+                    label={skill}
+                    color="secondary"
+                    variant="outlined"
+                    size="small"
+                  />
+                ))}
+              </Box>
+            </div>
+          </div>
+          <Divider sx={{ mx: 2 }} />
+          <div className="section connect-container">
+            <div>
+          <Button variant="outlined" size="large" onClick={() => handleConnectVolunteer(profileDetails.user._id)}>Connect</Button>
+            </div>
+          </div>
+        </div>
+  )
+}
+
+export default IndividualVolunteer
+
