53f81dc771622ae4f79fc4fc09478bf74cf77806
Umang Patel <umangpatel1991@gmail.com>
2021-11-26 00:01:43 -0500

    make axios call to update profile details

--- client/src/actions/profileActions.js
+++ client/src/actions/profileActions.js
@@ -3,6 +3,9 @@ import {
   PROFILE_DETAILS_FAIL,
   PROFILE_DETAILS_REQUEST,
   PROFILE_DETAILS_SUCCESS,
+  PROFILE_UPDATE_FAIL,
+  PROFILE_UPDATE_REQUEST,
+  PROFILE_UPDATE_SUCCESS,
 } from "../constants/profileConstants";
 
 export const showProfileDetails = () => async (dispatch, getState) => {
@@ -17,5 +20,30 @@ export const showProfileDetails = () => async (dispatch, getState) => {
     dispatch({ type: PROFILE_DETAILS_SUCCESS, payload: data });
   } catch (error) {
     dispatch({ type: PROFILE_DETAILS_FAIL, payload: error.message });
+  }
+};
+
+export const updateProfile = (updatedProfile) => async (dispatch, getState) => {
+  dispatch({ type: PROFILE_UPDATE_REQUEST });
+  const {
+    userSignin: { userInfo },
+  } = getState();
+  try {
+    const { data } = await Axios.put(
+      `/api/profiles/${updatedProfile._id}`, updatedProfile,
+      {
+        headers: { Authorization: `Bearer ${userInfo.token}` },
+      }
+    );
+    dispatch({
+      type: PROFILE_UPDATE_SUCCESS,
+      payload: data.updatedProfile
+    });
+  } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    dispatch({ type: PROFILE_UPDATE_FAIL, payload: message });
   }
 };
